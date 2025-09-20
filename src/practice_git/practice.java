0026db2bfa4dd0f72379c5571bcc4a07c9f129ed
shahidgsa <shahidgsa7@gmail.com>
2025-09-20 15:26:34 +0530

    added new class

---
+++ src/practice_git/practice.java
@@ -0,0 +1,12 @@
+package practice_git;
+
+public class practice {
+
+	public static void git1() {
+		System.out.println("first git ");
+	}
+
+	public static void main(String[] args) {
+		git1();
+	}
+}
